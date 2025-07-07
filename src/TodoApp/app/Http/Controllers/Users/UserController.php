2062be26bcd2cc4e751ecb824714aacd01f72b2b
Yamashita-Ryutaro <ryutaro.yamashita@g.dreamcareer.co.jp>
2025-07-07 16:39:27 +0900

    ログイン、新規登録画面

--- src/TodoApp/app/Http/Controllers/Users/UserController.php
+++ src/TodoApp/app/Http/Controllers/Users/UserController.php
@@ -10,11 +10,11 @@ class UserController extends Controller
     //
     public function showLoginPage()
     {
-
+        return view('user.login');
     }
 
     public function showRegisterPage()
     {
-        
+        return view('user.register');
     }
 }
--- src/TodoApp/resources/views/user/login.blade.php
+++ src/TodoApp/resources/views/user/login.blade.php
@@ -7,48 +7,43 @@
 <!--
 *   section：子ビューにsectionでデータを定義する
 *   セクション名：content を指定
-*   用途：会員登録ページを表示する
+*   用途：ログインページを表示する
 -->
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col col-md-offset-3 col-md-6">
             <nav class="panel panel-default">
-                <div class="panel-heading">会員登録</div>
+                <div class="panel-heading">ログイン</div>
                 <div class="panel-body">
                     @if($errors->any())
                         <div class="alert alert-danger">
-                            <ul>
-                                @foreach($errors->all() as $message)
-                                    <li>{{ $message }}</li>
-                                @endforeach
-                            </ul>
+                        <ul>
+                            @foreach($errors->all() as $message)
+                                <li>{{ $message }}</li>
+                            @endforeach
+                        </ul>
                         </div>
                     @endif
-                    <form action="{{ route('user.register') }}" method="POST">
+                    <form action="{{ route('user.login') }}" method="POST">
                         @csrf
                         <div class="form-group">
                             <label for="email">メールアドレス</label>
                             <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}" />
                         </div>
                         <div class="form-group">
-                            <label for="name">ユーザー名</label>
-                            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" />
-                        </div>
-                        <div class="form-group">
                             <label for="password">パスワード</label>
-                            <input type="password" class="form-control" id="password" name="password">
+                            <input type="password" class="form-control" id="password" name="password" />
                         </div>
-                        <div class="form-group">
-                            <label for="password-confirm">パスワード（確認）</label>
-                            <input type="password" class="form-control" id="password-confirm" name="password_confirmation">
-                        </div>
-                        <div class="text-right">
+                            <div class="text-right">
                             <button type="submit" class="btn btn-primary">送信</button>
                         </div>
                     </form>
                 </div>
             </nav>
+            <div class="text-center">
+                <a href="#">パスワードの変更はこちらから</a>
+            </div>
         </div>
     </div>
 </div>
