4b9b91fe7480cd569d33ff58465860cacfe4ae47
Yamashita-Ryutaro <ryutaro.yamashita@g.dreamcareer.co.jp>
2025-07-07 17:54:59 +0900

    ホームページの修正

--- src/TodoApp/app/Http/Controllers/HomeController.php
+++ src/TodoApp/app/Http/Controllers/HomeController.php
@@ -2,18 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Folder;
+use App\Services\Task\TaskService;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    protected $taskService;
+
+    public function __construct(TaskService $taskService)
+    {
+        $this->taskService = $taskService;
+    }
+
     /**
      *  ホームページを表示するコントローラー
      *  
      *  GET /
-     *  @return \Illuminate\View\View
+     *  @return \Illuminate\View\View|\Illuminate\Http\RedirectResponse
      */
     public function showHomePage()
     {
-        return view('home');
+        $user_id = Auth::id();
+        
+        // ログインユーザーのuser_idがFolderテーブルに存在するか判定
+        $folder = Folder::where('user_id', $user_id)->first();
+
+        if ($folder) {
+            return redirect()->route('tasks.index', ['id' => $folder->id]);
+        } else {
+            return view('home');
+        }
     }
 }
