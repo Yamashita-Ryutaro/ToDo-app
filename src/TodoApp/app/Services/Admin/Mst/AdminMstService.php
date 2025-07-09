09e9b1963b8ddb33b5db120760538b0ff33a8179
Yamashita-Ryutaro <ryutaro.yamashita@g.dreamcareer.co.jp>
2025-07-09 18:39:51 +0900

    マスタテーブル詳細ページの表示

--- src/TodoApp/app/Services/Admin/Mst/AdminMstService.php
+++ src/TodoApp/app/Services/Admin/Mst/AdminMstService.php
@@ -6,9 +6,15 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Password;
 use App\Models\Admin\MstTables;
 use App\Models\Admin\MstAdmin;
+use App\Models\Task\MstTaskStatus;
 
 class AdminMstService
 {
+    /**
+     * マスタテーブル一覧ページのデータを取得
+     *
+     * @return array
+     */
     public function showMstIndexPageData()
     {
         $mstTables = MstTables::all();
@@ -18,8 +24,42 @@ class AdminMstService
         ];
     }
 
+    /**
+     * マスタテーブル詳細ページのデータを取得
+     *
+     * @param string $table_name
+     * @return array|null
+     */
     public function showMstDetailPageData($table_name)
     {
+        $table = MstTables::where('table_name', $table_name)->first();
+        $contents = $this->getTableData($table_name);
+        return [
+            'table' => $table,
+            'contents' => $contents,
+        ];
+    }
 
+    /**
+     * 指定されたテーブルのデータを取得
+     *
+     * @param string $table_name
+     * @return \Illuminate\Database\Eloquent\Collection|null
+     */
+    private function getTableData($table_name)
+    {
+        switch ($table_name) {
+            case 'mst_admin':
+                $contents = MstAdmin::all();
+                break;
+            case 'mst_task_statuses':
+                $contents = MstTaskStatus::all();
+                break;
+            default:
+                Log::error("Unsupported table name: {$table_name}");
+                return null;
+        }
+
+        return $contents;
     }
 }
--- src/TodoApp/resources/views/admin/mst/detail.blade.php
+++ src/TodoApp/resources/views/admin/mst/detail.blade.php
@@ -11,6 +11,23 @@
 -->
 @section('content')
 <div class="container">
-
+    <div class="row">
+        <nav class="panel panel-default">
+            <div class="panel-heading">マスタテーブル詳細</div>
+            <div class="panel-body">
+                <p>テーブル名: {{ $table->table_name }}</p>
+                @if (@isset($contents))
+                    <p>表示名一覧:</p>
+                    <ul>
+                        @foreach($contents as $content)
+                            <li>
+                                {{ $content->display_name }}
+                            </li>
+                        @endforeach
+                    </ul>
+                @endif
+            </div>
+        </nav>
+    </div>
 </div>
 @endsection
