69946bdff6c7401c370de2a74d9c2ca649883469
Yamashita-Ryutaro <ryutaro.yamashita@g.dreamcareer.co.jp>
2025-07-10 18:23:37 +0900

    前のコミットは使うことにした

---
+++ src/TodoApp/database/migrations/2025_07_10_092121_create_notifications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject');
+            $table->text('body');
+            $table->string('url_key')->nullable();
+            $table->string('action_text')->nullable();
+            $table->bigInteger('notification_id')->unsigned()->unique();
+            $table->foreign('notification_id')->references('id')->on('mst_notifications');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
