34f17c94ac2dc32b86e595d00f73cdf54f3bca0c
Yamashita-Ryutaro <ryutaro.yamashita@g.dreamcareer.co.jp>
2025-07-10 17:12:50 +0900

    微修正

--- src/TodoApp/resources/views/tasks/index.blade.php
+++ src/TodoApp/resources/views/tasks/index.blade.php
@@ -10,10 +10,7 @@
 *   用途：javascriptライブラリー「flatpickr」のスタイルシートを指定
 -->
 @section('styles')
-    <!-- 「flatpickr」の デフォルトスタイルシートをインポート -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-    <!-- 「flatpickr」の ブルーテーマの追加スタイルシートをインポート -->
-    <link rel="stylesheet" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
+  @include('share.flatpickr.styles')
 @endsection
 
 <!--
